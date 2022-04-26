afdd98d4e25338cc2fa6c5eacff90f7a5467bc64
physwizz <pollockg86@gmail.com>
2022-04-26 16:20:34 +1000

    v6.2

--- drivers/gpu/arm/b_r26p0/platform/exynos/gpu_control.c
+++ drivers/gpu/arm/b_r26p0/platform/exynos/gpu_control.c
@@ -48,6 +48,8 @@ unsigned int gpu_pmu_status_reg_offset;
 unsigned int gpu_pmu_status_local_pwr_mask;
 #define EXYNOS_PMU_G3D_STATUS	gpu_pmu_status_reg_offset
 #define LOCAL_PWR_CFG			gpu_pmu_status_local_pwr_mask
+unsigned int gpu_min_override = 377000;
+unsigned int gpu_max_override = 1196000;
 
 #ifdef CONFIG_MALI_RT_PM
 static struct exynos_pm_domain *gpu_get_pm_domain(char *g3d_genpd_name)
